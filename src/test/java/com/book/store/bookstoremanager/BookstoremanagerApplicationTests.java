a249062d3530d8f369aef9479a4113ee44786d6f
Guilherme de Castro <42876310+guicastrofer@users.noreply.github.com>
2020-06-11 11:04:51 -0300

    first commit

---
+++ src/test/java/com/book/store/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.store.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
